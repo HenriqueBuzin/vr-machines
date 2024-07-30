b910e600cd9796fd634538d1a0bc1ea96c52a373
Henrique Buzin <cultura.henrique@gmail.com>
2024-07-30 20:25:01 -0300

    Parabola

--- Assets/Scripts/Graph/VoltageGraph.cs
+++ Assets/Scripts/Graph/VoltageGraph.cs
@@ -37,6 +37,73 @@ public class VoltageGraph : MonoBehaviour
         return gameObject;
     }
 
+    private void CreateBezierCurve(Vector2 pointA, Vector2 pointB, Vector2 controlPointA, Vector2 controlPointB, Color color)
+    {
+        int segmentCount = 20; // Número de segmentos para a curva
+
+        Vector2 previousPoint = pointA;
+        for (int i = 1; i <= segmentCount; i++)
+        {
+            float t = i / (float)segmentCount;
+            Vector2 currentPoint = CalculateBezierPoint(t, pointA, controlPointA, controlPointB, pointB);
+
+            CreateLine(previousPoint, currentPoint, color);
+
+            previousPoint = currentPoint;
+        }
+    }
+
+    private Vector2 CalculateBezierPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+
+        Vector2 p = uuu * p0; // primeiro ponto
+        p += 3 * uu * t * p1; // primeiro ponto de controle
+        p += 3 * u * tt * p2; // segundo ponto de controle
+        p += ttt * p3; // último ponto
+
+        return p;
+    }
+
+    private void ShowGraph(List<float> valueList)
+    {
+        float graphHeight = graphContainer.sizeDelta.y;
+        float yMaximum = 50f;
+        float xSize = 50f;
+
+        GameObject lastDot = null;
+        Vector2 lastDotPosition = Vector2.zero;
+
+        for (int i = 0; i < valueList.Count; i++)
+        {
+            float xPosition = xSize + i * xSize;
+            float yPosition = (valueList[i] / yMaximum) * graphHeight;
+            Vector2 dotPosition = new Vector2(xPosition, yPosition);
+            bool overMax = valueList[i] > maxVoltage;
+
+            GameObject dot = CreateDot(dotPosition, overMax);
+
+            if (lastDot != null)
+            {
+                Vector2 nextDotPosition = dotPosition;
+
+                // Calcula os pontos de controle para a curva
+                Vector2 controlPointA = lastDotPosition + new Vector2(xSize / 2, 0);
+                Vector2 controlPointB = nextDotPosition - new Vector2(xSize / 2, 0);
+
+                Color currentLineColor = valueList[i] > maxVoltage ? lineColorOverMax : lineColor;
+                CreateBezierCurve(lastDotPosition, nextDotPosition, controlPointA, controlPointB, currentLineColor);
+            }
+
+            lastDot = dot;
+            lastDotPosition = dotPosition;
+        }
+    }
+
     private void CreateLine(Vector2 pointA, Vector2 pointB, Color color)
     {
         GameObject lineObj = new GameObject("line", typeof(Image));
@@ -53,33 +120,6 @@ public class VoltageGraph : MonoBehaviour
         rectTransform.sizeDelta = new Vector2(distance, 3f); // Ajuste a largura da linha conforme necessário
         rectTransform.anchoredPosition = pointA + direction * distance * 0.5f;
         rectTransform.localRotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-    }
-
-    private void ShowGraph(List<float> valueList)
-    {
-        float graphHeight = graphContainer.sizeDelta.y;
-        float yMaximum = 50f;
-        float xSize = 50f;
-
-        GameObject lastDot = null;
-        for (int i = 0; i < valueList.Count; i++)
-        {
-            float xPosition = xSize + i * xSize;
-            float yPosition = (valueList[i] / yMaximum) * graphHeight;
-            Vector2 dotPosition = new Vector2(xPosition, yPosition);
-            bool overMax = valueList[i] > maxVoltage;
-
-            GameObject dot = CreateDot(dotPosition, overMax);
-
-            if (lastDot != null)
-            {
-                Vector2 lastDotPosition = lastDot.GetComponent<RectTransform>().anchoredPosition;
-                Color currentLineColor = valueList[i] > maxVoltage ? lineColorOverMax : lineColor;
-                CreateLine(lastDotPosition, dotPosition, currentLineColor);
-            }
-
-            lastDot = dot;
-        }
     }
 
     private void CreateYAxisLabels(float yMax, float yInterval)
